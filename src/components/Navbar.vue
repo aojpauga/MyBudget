46389bb0d340ed0eee19017de5a7a6183328a7a0
AJ <aojpauga@gmail.com>
2020-02-26 15:43:41 -0700

    updated

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+<nav>
+    <v-toolbar color="primary">
+        <v-toolbar-title class="text-uppercase white--text">
+            <span text-color="white" class="font-weight-light">my</span>
+            <span color="primary">Budget</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn outline color="#E3F2FD" class="ma-2" router to="/Home">Home</v-btn>
+        <v-btn outline color="#E3F2FD" class="ma-2" router to="/About">MyData</v-btn>
+    </v-toolbar>
+</nav>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container grid-list-xs>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-text-field name="funds" label="Enter Income" id="funds"></v-text-field>
+      </v-flex>
+      <v-flex xs12>
+        <v-btn outline color="primary" dark>Enter</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card xs12 class="mx-auto" max-width="400">
+          <div class="ma-3">Income</div>
+          <div>
+            <v-row>
+              <v-text-field box class="ma-3" label="Check 1"></v-text-field>
+              <v-checkbox label="Paid" v-model="value" value="value"></v-checkbox>
+            </v-row>
+          </div>
+          <div></div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
