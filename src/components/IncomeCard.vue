b0549475fd6a04301de7002cf88ad9ac091f7b99
AJ <aojpauga@gmail.com>
2020-03-10 13:45:56 -0600

    login component/screen added

--- src/components/IncomeCard.vue
+++ src/components/IncomeCard.vue
@@ -1,22 +1,5 @@
 <template>
-  <v-layout row wrap justify-space-around>
-    <v-flex xs12>
-      <v-card xs12 class="mx-auto" max-width="500">
-        <div class="ma-3">Income</div>
-        <div>
-          <v-row class="mx-auto">
-            <v-text-field box class="ma-3" label="Check 1"></v-text-field>
-            <v-checkbox
-              class="ma-3"
-              label="Paid"
-              v-model="value"
-              value="value"
-            ></v-checkbox>
-          </v-row>
-        </div>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  
 </template>
 
 <script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,15 @@
 <template>
-    
+  <LoginComponent />
 </template>
+  
+
 
 <script>
+import LoginComponent from "../components/Login";
+
 export default {
-    
-}
+  components: {
+    LoginComponent
+  }
+};
 </script>
