28d58fabdbafba73ca6fc07d37600a10382b3a50
AJ <aojpauga@gmail.com>
2020-03-28 13:20:16 -0600

    added delete

---
+++ src/components/DeleteIncomeDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500" :retain-focus="false">
+      <template v-slot:activator="{ on }">
+        <v-btn color="red lighten-2" dark v-on="on">Delete</v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>Delete</v-card-title>
+
+        <v-card-text>Are you sure you want to delete this item?</v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,10 +18,40 @@
             :key="card.title"
           >
             <div class="ma-3">{{ card.title }}</div>
+            <div>{{card.id}}</div>
             <div>
               <v-row class="mx-auto">
                 <v-text-field class="ma-3" :label="card.inputLabel" v-model="card.amount"></v-text-field>
+
                 <v-checkbox class="ma-3" label="Paid"></v-checkbox>
+                <div class="text-center">
+                  <v-dialog v-model="dialog" width="500" :retain-focus="false">
+                    <template v-slot:activator="{ on }">
+                      <v-btn color="red lighten-2" dark v-on="on" @click="incomeID=card.id">Delete</v-btn>
+                    </template>
+
+                    <v-card>
+                      <v-card-title class="headline grey lighten-2" primary-title>Delete</v-card-title>
+
+                      <v-card-text>Are you sure you want to delete this item?</v-card-text>
+                      <v-text-field v-model="incomeID"></v-text-field>
+
+                      <v-divider></v-divider>
+
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          color="primary"
+                          text
+                          @click="dialog = false"
+                          v-on:click.native="deleteIncome(incomeID)"
+                        >Delete</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </div>
+
+                <!-- <DeleteIncomeDialog /> -->
               </v-row>
             </div>
           </v-card>
@@ -43,6 +73,9 @@
               <v-row class="mx-auto">
                 <v-text-field class="ma-3" :label="card.inputLabel" v-model="card.amount"></v-text-field>
                 <v-checkbox class="ma-3" label="Paid"></v-checkbox>
+                <v-btn class="mx-2" fab dark small color="error">
+                  <v-icon dark>mdi-minus</v-icon>
+                </v-btn>
               </v-row>
             </div>
           </v-card>
@@ -55,6 +88,7 @@
 <script>
 import AddIncomeDialog from "../components/AddIncomeDialog";
 import AddExpenseDialog from "../components/AddExpenseDialog";
+//import DeleteIncomeDialog from "../components/DeleteIncomeDialog";
 import db from "@/fb.js";
 export default {
   data() {
@@ -63,7 +97,9 @@ export default {
       incomeTitle: "",
       incomeCards: [],
       expenseCards: [],
-      count: 1
+      count: 1,
+      dialog: false,
+      incomeID: ""
     };
   },
   methods: {
@@ -79,6 +115,17 @@ export default {
         inputLabel: "Income " + this.count,
         cardIncome: 0
       });
+    },
+    deleteIncome: function(id) {
+      db.collection("income")
+        .doc(id)
+        .delete()
+        .then(function() {
+          console.log("Document successfully deleted!");
+        })
+        .catch(function(error) {
+          console.error("Error removing document: ", error);
+        });
     }
   },
   created() {
@@ -91,6 +138,12 @@ export default {
             ...change.doc.data(),
             id: change.doc.id
           });
+        } else if (change.type === "removed") {
+          var id = change.doc.id;
+          console.log(id);
+          for (var card in this.incomeCards) {
+            this.incomeCards.splice(card, 1);
+          }
         }
       });
     });
@@ -111,6 +164,7 @@ export default {
   components: {
     AddIncomeDialog,
     AddExpenseDialog
+    //DeleteIncomeDialog
   }
 };
 </script>
